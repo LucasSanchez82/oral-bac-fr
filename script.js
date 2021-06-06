d6100e27c6a6355a024b923746d5aa0ab1289f52
Lucas Sanchez <08.lucas.sanchez@gmail.com>
2021-06-06 07:15:13 +0200

    refresh update

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 fetch('data.json')
 .then(response => response.json())
 .then(data => {
-
+    function mouvement(date) {
+        console.log(date);
+        if (1549<date && date<1570) {
+            return "pléiade"
+        }else if(1570<date && date<1600){
+            return "baroque"
+        }else if(1650<date && date<1700){
+            return "classicisme"
+        }else if(1720<date && date<1770){
+            return "les lumières"
+        }else if(1820<date && date<1865){
+            return "le romantisme"
+        }else if(1818<date && date<1865){
+        //     return "le parnasse"
+        // }else if(1866<date && date<1875){
+            return "le realisme"
+        }else if(1875<date && date<1885){
+            return "le naturalisme"
+        }else if(1885<date && date<1900){
+            return "le symbolisme"
+        }else if(1900<date && date<1923){
+            return "Le dadaisme"
+        }else if(1923<date && date<1940){
+            return "le surrealisme"
+        }else{
+            return "absurde"
+        }
+        
+    }
     for (let i = 0; i < data.length; i++) {
         // document.querySelector('#container').innerHTML += '<br />';
         document.querySelector('#container').innerHTML += (
@@ -10,9 +38,9 @@ fetch('data.json')
                 <h1>${data[i].title}</h1> 
                 <h2>${data[i].auteur}</h2>
                 <h3>${data[i].date} </h3>
-                <h4>${data[i].mouvement}</h4>
+                <h4>${mouvement(data[i].date)}</h4>
                 <h5>${data[i].genre}</h5>
-                <h6>${data[i].extrait.situer} </h6>
+                <h6>${data[i].extrait.situer}</h6>
                 <div class='blockImp'>
                     <div class='blockItem'> <span class='theme'>${data[i].theme}</span> </div>
                     <div class='blockItem'> <span class='problematique'>${data[i].problematique}</span> </div>
@@ -64,8 +92,8 @@ fetch('data.json')
     function lineBreak(module, number, change) {
         let text = document.querySelectorAll(module)[number].innerText.split(change).join('\n')
         document.querySelectorAll(module)[number].innerText = text
+        if(document.querySelectorAll(module)[number+1]) lineBreak(module, number+1, change)
     }
-    lineBreak('.plan', 1, '--->')            
-    lineBreak('.plan', 2, '--->')            
-    lineBreak('.plan', 3, '--->')            
+    lineBreak('.plan', 0, '--->')            
+          
 })
