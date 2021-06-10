074ddded9a489c791a604dac712c089746d4d41b
Lucas Sanchez <08.lucas.sanchez@gmail.com>
2021-06-10 19:17:28 +0200

    refresh update

--- searchBar.js
+++ searchBar.js
@@ -1,6 +1,6 @@
 function searchBar() {
     // Declare variables
-    var input, filter, ul, block, h1, i, txtValue;
+    var input, filter, block, h1, h2,  i, txtValueH1, txtValueH2;
     block = document.querySelectorAll('.item'); // ✔
     input = document.getElementById('myInput'); // ✔
     filter = input.value.toUpperCase(); // ✔
@@ -9,8 +9,10 @@ function searchBar() {
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < block.length; i++) {
       h1 = block[i].getElementsByTagName("h1")[0];
-      txtValue = h1.textContent || h1.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      h2 = block[i].getElementsByTagName("h2")[0];
+      txtValueH1 = h1.textContent || h1.innerText ;
+      txtValueH2 = h2.textContent || h2.innerText ;
+      if (txtValueH1.toUpperCase().indexOf(filter) > -1 || txtValueH2.toUpperCase().indexOf(filter) > -1) {
         block[i].style.display = "";
       } else {
         block[i].style.display = "none";
